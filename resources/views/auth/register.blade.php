7374da9373239716603152f2fa96ee49eedc8ed9
Joynal Abedin <joynal3483@gmail.com>
2015-11-07 20:18:54 +0600

    gitignore fixed :D

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,42 @@
+<form method="POST" action="/auth/register">
+    {!! csrf_field() !!}
+
+    <div>
+        Name
+        <input type="text" name="name" value="{{ old('name') }}">
+    </div>
+    <div>
+        Username
+        <input type="text" name="username" value="{{ old('username') }}">
+    </div>
+
+    <div>
+        Email
+        <input type="email" name="email" value="{{ old('email') }}">
+    </div>
+
+    <div>
+        Password
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        Confirm Password
+        <input type="password" name="password_confirmation">
+    </div>
+
+    <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
+
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
